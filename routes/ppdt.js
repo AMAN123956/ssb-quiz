b76ffe7b74f84998c8c611189c7604b15e0b05cd
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-05-31 08:15:35 +0530

    Added ppdt routes

---
+++ routes/ppdt.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const { ppdthome } = require('../controllers/ppdt/home')
+const {test,savetest,savereturn,ppdtresult} = require('../controllers/ppdt/final')
+// PPDT HOME PAGE 
+router.route('/home').get(ppdthome)
+
+// PPDT TEST PAGE 
+router.route('/test').get(test)
+router.route('/result').get(ppdtresult)
+
+router.route('/savetest').post(savetest)
+router.route('/savereturn').post(savereturn)
+
+
+
+
+
+
+
+// Router Exported
+module.exports = router
